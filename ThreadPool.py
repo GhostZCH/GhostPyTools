2de6301e3eb77657c472b13e8a936cd9aefb1319
GhostZCH <zhao_chenhui_scu@163.com>
2015-08-19 08:43:09 +0800

    ThreadPool

--- ThreadPool.py
+++ ThreadPool.py
@@ -6,14 +6,14 @@ console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 logging.getLogger().addHandler(console)
 
-class IWork:
+class IWork(object):
     def __init__(self):
         pass
 
     def run(self):
         pass
 
-    def finish(self):
+    def on_finish(self):
         pass
 
 
@@ -26,22 +26,30 @@ class ThreadWorker(threading.Thread):
 
     def run(self):
         logging.info('ThreadWorker.run: %s' % self._id)
-        print('ThreadWorker.run: %s' % self._id)
-        work = self._get_work()
-        while work:
+
+        while True:
+            try:
+                work = self._get_work()
+            except Queue.Empty:
+                continue
+
+            if not work:
+                return
+
             logging.info('ThreadWorker.run.work.run: %s' % self._id)
             work.run()
-            work.finish()
-            work = self._get_work()
+            work.on_finish()
+
         logging.info('ThreadWorker.run.work == None, thread exit: %s' % self._id)
-        print('ThreadWorker.run.work == None, thread exit: %s' % self._id)
+
 
 class ThreadPool:
-    def __init__(self, max_size=4):
+    def __init__(self, max_size=4, max_queue_size=1024):
         self._pool = []
-        self._que = Queue.Queue(maxsize=1024)
+        self._que = Queue.Queue(maxsize=max_queue_size)
         self._lock = threading.Lock()
         self._is_stop = False
+        self._is_wait = False
 
         for i in xrange(max_size):
             worker = ThreadWorker(self._get_work, i)
@@ -53,14 +61,29 @@ class ThreadPool:
             return None
 
         with self._lock:
-            return self._que.get()
+            try:
+                return self._que.get_nowait()
+            except Queue.Empty, ex:
+                if self._is_wait:
+                    return None
+                raise ex
 
     def add_work(self, work):
         if isinstance(work, IWork):
             self._que.put(work)
             return
 
-        raise Exception('parm error: work mush a instance of IWork, your worke is a %s' % type(work))
+        raise Exception('parm error: work mush a instance of IWork, your work is a %s' % type(work))
 
     def stop(self):
         self._is_stop = True
+
+    def wait(self):
+        logging.warn('ThreadPool.wait: begin wait ')
+        self._is_wait = True
+        is_run = True
+        while is_run:
+            is_run = False
+            for th in self._pool:
+                is_run |= th.isAlive()
+        logging.warn('ThreadPool.wait: all thread exit. ')
--- ThreadsPoolTest.py
+++ ThreadsPoolTest.py
@@ -1,58 +1,56 @@
+
+import time
+import logging
 import ThreadPool
 import unittest
-import logging
-import threading
-import time
 
-import Queue
-
-print('xxxxxxxxxxxxxxx')
 console = logging.StreamHandler()
-console.setLevel(logging.INFO)
 logging.getLogger().addHandler(console)
+logging.getLogger().setLevel(logging.ERROR)
 
 class TestWork(ThreadPool.IWork):
-    def __init__(self, n, handler):
+    def __init__(self, n):
+        ThreadPool.IWork.__init__(self)
         self._n = n
         self._result = 1
-        self._handler = handler
 
     def run(self):
         # compute n!
         for number in xrange(1, self._n + 1):
             self._result *= number
 
-    def finish(self):
-        self._handler(self._result)
+    def on_finish(self):
+        # print(self._result)
+        pass
+
 
 class TPoolTest(unittest.TestCase):
-    def write_ourput(self, result):
-        with self. _mutex:
-            print('%s: %s' % (self._count,  result))
-            self._count += 1
-
     def setUp(self):
-        self._mutex = threading.Lock()
-        self._count = 0
+        pass
 
     def tearDown(self):
         pass
 
-    def test_4_threads(self):
-        work_num = 1024
-        pool = ThreadPool.ThreadPool()
-
+    def pool_test(self, thread_num):
+        work_num = 1024  # total work num
+        start = time.clock()
+        pool = ThreadPool.ThreadPool(thread_num)
         for i in xrange(work_num):
-            pool.add_work(TestWork(100, self.write_ourput))
+            pool.add_work(TestWork(1000))  # compute 1000!
+        pool.wait()
+        print '%4d  threads: %0.6f seconds' % (thread_num, time.clock() - start)
 
-        que = Queue.Queue()
-        x = que.get()
-
-        while self._count < work_num:
-            time.sleep(0.1)
-        else:
-            pool.stop()
-            print('over')
+    def test_threads(self):
+        self.pool_test(1)
+        self.pool_test(4)
+        self.pool_test(8)
+        self.pool_test(12)
+        self.pool_test(13)
+        self.pool_test(14)
+        self.pool_test(15)
+        self.pool_test(16)
+        self.pool_test(32)
+        self. pool_test(1024)
 
 if __name__ == '__main__':
     unittest.main()
